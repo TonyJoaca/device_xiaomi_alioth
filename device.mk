bd1e452532caf21eed5c62fde2892416624fcb0e
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-01-24 15:22:12 +0530

    alioth: Set default Settings.Global.DEVICE_NAME according to MIUI
    * MIUI uses ro.product.vendor.marketname for it
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -43,6 +43,11 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    SettingsProviderM2012K11AC \
+    SettingsProviderM2012K11AG \
+    SettingsProviderM2012K11AI
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
